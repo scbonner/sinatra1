5e83969a5573b614c43f4f09fed8644b6d654068
scbonner <codingbootcamp918@gmail.com>
2020-02-10 01:59:28 +0000

    starting from scratch

--- sinatra1.rb
+++ sinatra1.rb
@@ -4,37 +4,37 @@
 # # The section of the program would look like this, with a
 # # multi-line string for all the html:
 
-require 'sinatra'
+# require 'sinatra'
  
-get "/getdoc" do
-  "<!DOCTYPE html>
-  <html>
-  <style>
-  body {
-  color: red; 
-  }
-  </style>
-  <body>
-    <h1>Hello There</h1>
-    <p>
-      … your stuff goes here ..
-    </p>
-  </body>
-  </html>"
-end
- 
+# get "/getdoc" do
+#   "<!DOCTYPE html>
+#   <html>
+#   <style>
+#   body {
+#   color: red; 
+#   }
+#   </style>
+#   <body>
+#     <h1>Hello There</h1>
+#     <p>
+#       … your stuff goes here ..
+#     </p>
+#   </body>
+#   </html>"
+# end
+require "sinatra"
 get '/' do
   "Hello, World!"
 end
 
-a = "yes"
-get "/other" do
-  if a == "yes"
-    "<h1>Yes</h1>"
-  else
-    "<h1>No</h1>"
-  end
-end
+# a = "yes"
+# get "/other" do
+#   if a == "yes"
+#     "<h1>Yes</h1>"
+#   else
+#     "<h1>No</h1>"
+#   end
+# end
 
 # post "/submitted" do
 #   "I'm getting there: #{params['message']}" 
