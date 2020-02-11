e8b83d6f5ead0b7c30446424af6bbf0e9a5cf8ba
scbonner <codingbootcamp918@gmail.com>
2020-02-11 04:34:45 +0000

    complete Sinatra Exercise 1

---
+++ main.rb
@@ -0,0 +1,8 @@
+# extra practice under Sitepoint
+
+
+
+require 'sinatra' 
+get '/' do 
+  "Just Do It" 
+end
--- public/frank_says.rb
+++
@@ -1,7 +0,0 @@
-
-require 'sinatra1'
-    get '/frank-says' do
-      'Put this in your pipe & smoke it!'
-    end
-    
-    
